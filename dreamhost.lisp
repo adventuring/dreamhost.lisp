f06e3c919b4a2fa8d10c128cc0e51dbbd0931d79
Bruce-Robert Pocock <brpocock@star-hope.org>
2019-04-05 11:23:35 -0400

    format, not much

--- dreamhost.lisp
+++ dreamhost.lisp
@@ -34,6 +34,7 @@ https://panel.dreamhost.com/?tree=home.api")
 (define-condition invalid-record (dreamhost-api-error-with-details) ())
 (define-condition invalid-type (dreamhost-api-error-with-details) ())
 (define-condition invalid-value (dreamhost-api-error-with-details) ())
+(define-condition slow-down-bucko (dreamhost-api-error-with-details) ())
 
 
 
@@ -56,6 +57,14 @@ https://panel.dreamhost.com/?tree=home.api")
     value)
   (:method ((type (eql :aaaa)) (value string))
     value))
+
+(defun call-dreamhost (cmd params)
+  (let ((unique-id (uuid:format-as-urn nil (uuid:make-v4-uuid))))
+    (http-request (render-uri (make-uri-space  "https://api.dreamhost.com/") nil))
+    ;; key=$KEY&unique_id=$UUID&cmd=$CMD&ps=$PS
+    ))
+
+
 
 (defun dns-add-record (name type &optional value comment)
   "Add a DNS record for NAME of TYPE; VALUE is determined by TYPE.
@@ -95,15 +104,19 @@ internal_error_could_not_add_record"
   "
 dns-list_records
 
-Dump a list of all DNS records for all domains (not including registrations)
-on all accounts you have access to.  Please note that this skips the
-dreamhosters.com, dreamhost.com, dreamhostps.com, and newdream.net zones.
+Dump  a  list  of  all  DNS  records  for  all  domains  (not  including
+registrations) on all accounts you have access to. Please note that this
+skips   the   dreamhosters.com,  dreamhost.com,   dreamhostps.com,   and
+newdream.net zones.
 
 Command 	dns-list_records
+
 Values 	(none)
+
 Result
 
 success
+@code
 account_id zone record type value comment editable
 1 718apts.com 718apts.com A 1.2.3.4 0
 1 718apts.com 718apts.com MX 0 mx1.balanced.dreamhost.com. 0
@@ -112,33 +125,50 @@ account_id zone record type value comment editable
 1 718apts.com 718apts.com NS ns2.dreamhost.com. 0
 1 718apts.com 718apts.com NS ns3.dreamhost.com. 0
 1 718apts.com test.718apts.com CNAME ghs.google.com. A test I did. 1
-Possible values 	type : A,MX,NS,CNAME,PTR,NAPTR,TXT,SRV,AAAA, or A6
+@end code
+
+Possible values 
+	
+type : A,MX,NS,CNAME,PTR,NAPTR,TXT,SRV,AAAA, or A6
 
 editable : 0 or 1
+
 Possible errors 	(none)
 ")
 
 
 
 (defun dns-remove-record (name)
-  "Commands
-dns-remove_record
+  "Removes an existing editable DNS record you have with DreamHost.
 
-Removes an existing editable DNS record you have with DreamHost.  However,
-you cannot remove dreamhosters.com records.  Keep in mind DNS changes may
-take a while to propagate.
+Command: dns-remove_record
 
- Command dns-remove_record Values
+However, you cannot remove dreamhosters.com  records. Keep in mind DNS
+changes may take a while to propagate.
 
-record : The full name of the record you'd like to remove, e.g., testing.groo.com
-type : The type (see dns-add_record) of the record you'd like to remove.
-value : The value (see dns-add_record) of the record you'd like to remove.
+Command
+ 
+dns-remove_record 
+
+Values
+
+record:  The  full name  of  the  record  you'd  like to  remove,  e.g.,
+testing.groo.com
+
+type: The type (see dns-add_record) of the record you'd like to remove.
+
+value:  The  value  (see  dns-add_record)   of  the  record  you'd  like
+to remove.
+
 Result
 
 success
+
 record_removed
+
 Possible errors
 
+@code
 no_record
 no_type
 no_value
@@ -148,6 +178,7 @@ no_such_value
 not_editable
 internal_error_could_not_destroy_record
 internal_error_could_not_update_zone
+@end code
 
 ")
 
@@ -172,23 +203,33 @@ Required
 key
 An API Key that you need to generate via the web panel.
 
-In order for a sub-account to create an API Key, it must be granted 'Billing' Account Privileges.
+In order  for a  sub-account to create  an API Key,  it must  be granted
+'Billing' Account Privileges.
 
-Please note that if a sub-account creates an API Key, it will only be
-visible to the sub-account on the API Key page in the panel.  The primary
-account owner will not be able to view the sub-account's API Key in their
-panel.
+Please note that  if a sub-account creates  an API Key, it  will only be
+visible to the sub-account on the API Key page in the panel. The primary
+account owner  will not  be able  to view the  sub-account's API  Key in
+their panel.
 
-cmd The command you'd like to run.  When you create your key you pick what
-command(s) it may access.  unique_id You may pass a new unique_id (max
-length: 64 chars) when you submit a request, to make sure you don't
-accidentally submit the same command twice.  Note that only successful
-queries \"use up\" a unique_id.  DreamHost recommends using UUIDs for this
-purpose if you'd like.  The unique_id you use only applies to your specific
-API key, so you never need to worry about any other users already \"using\"
-a specific unique_id.  Optional
+cmd 
+
+The  command you'd like  to run. When you  create your key  you pick
+what  command(s)   it  may  access.   
+
+unique_id
+  
+You may  pass a  new unique_id  (max length: 64  chars) when  you submit
+a request, to  make sure you don't accidentally submit  the same command
+twice.  Note  that  only  successful queries  \"use  up\"  a  unique_id.
+DreamHost  recommends  using  UUIDs  for this  purpose  if  you'd  like.
+The unique_id  you use  only applies  to your specific  API key,  so you
+never need to  worry about any other users already  \"using\" a specific
+unique_id. 
+
+Optional
 
 format
+
 The format you want to receive output in. Valid formats are:
 
 tab (default)
@@ -201,32 +242,33 @@ html
 
 account
 
-The account number to perform operations under.  Defaults to your own
+The account  number to  perform operations under.  Defaults to  your own
 account, or the first account you have access to otherwise.
 
 
 Rate limit
 
-In order to ensure that DreamHost systems remains stable and responsive, the
-rate at which certain API commands or groups of API commands can be run by
-the same user has been limited.  These limits are relatively generous and
-very few users ever run into them.  Limits are usually set per hour or per
-day, although some commands may have limits in shorter periods of time such
-as 10 minutes.  When API calls fail for any reason (for example if you tried
-to create a user that already existed), it does not count against your
-limits (note that this also means that failing because you exceeded the rate
-limit also does not count).  When you do run into a limit, the error
-returned is:
+In order to ensure that DreamHost systems remains stable and responsive,
+the rate at which certain API commands  or groups of API commands can be
+run  by the  same user  has been  limited. These  limits are  relatively
+generous and very  few users ever run into them.  Limits are usually set
+per hour or  per day, although some commands may  have limits in shorter
+periods  of  time such  as  10  minutes. When  API  calls  fail for  any
+reason (for example if you tried to create a user that already existed),
+it does  not count against your  limits (note that this  also means that
+failing  because you  exceeded  the  rate limit  also  does not  count).
+When you do run into a limit, the error returned is:
 
 error
+
 slow_down_bucko (detailed info about type of limit reached after a tab)
 
-If you run into this error, you should consider ways in which you could
-reduce the frequency that you are calling the API.  Most likely you would
-only run into these limits if you have a script or automated program that
-loops to repeatedly make API calls.  So you can simply slow down the rate at
-which your script runs, or make sure that it keeps track of how many
-commands it has issued in the last hour/day.
+If you run into this error, you  should consider ways in which you could
+reduce the frequency that you are calling the API. Most likely you would
+only run  into these limits  if you have  a script or  automated program
+that loops to repeatedly make API calls. So you can simply slow down the
+rate at which your script runs, or  make sure that it keeps track of how
+many commands it has issued in the last hour/day.
 
 Test Account
 
